20a6a39d4e5e10873e98139a8f15e304c6caee66
Michael Bastos <bastosmichael@gmail.com>
2018-07-01 22:18:13 -0500

    adding to readme docs

--- README.md
+++ README.md
@@ -15,14 +15,34 @@ Quick little Tic Tac Toe API game
 
 `curl --data "boardSize=3" http://localhost:8000/game/new`
 
+```{"message":"new game created",
+ "starting_turn":"X",
+ "output":"   |   |  \r\n-----------\r\n   |   |  \r\n-----------\r\n   |   |  ",
+ "position":"3  .  .  . \r\n2  .  .  . \r\n1  .  .  . \r\n   1  2  3  "}```
+
 ### Check Status of Board
 
 `curl http://localhost:8000/game/status`
+
+```{"message":"in progress",
+ "next_turn":"X",
+ "output":"   |   |  \r\n-----------\r\n   |   |  \r\n-----------\r\n   |   |  ",
+ "poistion":"3  .  .  . \r\n2  .  .  . \r\n1  .  .  . \r\n   1  2  3  "}```
 
 ### Make a Random Move
 
 `curl -X POST http://localhost:8000/game/random`
 
+```{"message":"in progress",
+ "next_turn":"O",
+ "output":"   |   |  \r\n-----------\r\n   |   |  \r\n-----------\r\n   |   | X",
+ "position":"3  .  .  . \r\n2  .  .  . \r\n1  .  .  X \r\n   1  2  3  "}```
+
 ### Make a Move using xAxis and yAxis parameters
 
-`curl --data "xAxis=1&yAxis=2" http://localhost:8000/game/new`
+`curl --data "xAxis=1&yAxis=2" http://localhost:8000/game/move`
+
+```{"message":"in progress",
+ "next_turn":"X",
+ "output":"   |   |  \r\n-----------\r\n O |   |  \r\n-----------\r\n   |   | X",
+ "position":"3  .  .  . \r\n2  O  .  . \r\n1  .  .  X \r\n   1  2  3  "}```
--- app/routes/game_routes.js
+++ app/routes/game_routes.js
@@ -6,26 +6,30 @@ module.exports = function(app) {
     app.locals.game = new TicTacToe.TicTacToe(req.body.boardSize || 3);  // default 3
     res.json({message: 'new game created',
               starting_turn: app.locals.game.turn(),
-              output: app.locals.game.ascii()})
+              output: app.locals.game.ascii(),
+              position: app.locals.game.ascii2()})
   });
 
   app.get('/game/status', (req, res) => {
     res.json({message: app.locals.game.status(),
               next_turn: app.locals.game.turn(),
-              output: app.locals.game.ascii()})
+              output: app.locals.game.ascii(),
+              poistion: app.locals.game.ascii2()})
   });
 
   app.post('/game/random', (req, res) => {
     app.locals.game.randomMove();
     res.json({message: app.locals.game.status(),
               next_turn: app.locals.game.turn(),
-              output: app.locals.game.ascii()})
+              output: app.locals.game.ascii(),
+              position: app.locals.game.ascii2()})
   });
 
   app.post('/game/move', (req, res) => {
     app.locals.game.move(req.body.xAxis, req.body.yAxis);
     res.json({message: app.locals.game.status(),
               next_turn: app.locals.game.turn(),
-              output: app.locals.game.ascii2()})
+              output: app.locals.game.ascii(),
+              position: app.locals.game.ascii2()})
   });
 };
